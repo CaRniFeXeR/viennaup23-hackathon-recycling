ba2d2099982e9e9db0e3c300ff922d0df0571089
Florian Pfiel <florian.pfiel@ace.at>
2023-06-02 20:38:36 +0200

    frontend added LineChart

--- frontend/openTrash_frontend/src/main.js
+++ frontend/openTrash_frontend/src/main.js
@@ -3,4 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import CanvasJSChart from '@canvasjs/vue-charts';
+
+const app = createApp(App);
+app.use(CanvasJSChart); // install the CanvasJS Vuejs Chart Plugin
+app.mount('#app')
