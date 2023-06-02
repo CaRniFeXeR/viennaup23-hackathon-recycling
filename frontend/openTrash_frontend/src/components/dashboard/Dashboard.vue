e3272333fd1c6eda31616345167ac61c00401fb6
Florian Pfiel <florian.pfiel@ace.at>
2023-06-02 18:59:12 +0200

    frontend update

---
+++ frontend/openTrash_frontend/src/components/dashboard/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+<h1>test</h1>
+    
+</template>
+
+
+<script>
+
+export default{
+    name: 'Dashboard',
+    props: {},
+    computed: {
+        // retrieve data here
+    }
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
