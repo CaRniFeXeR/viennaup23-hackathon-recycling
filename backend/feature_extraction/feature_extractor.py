d0e4276f82880fd978cb021aec77f9cc4fbb1705
Alireza Furutanpey <reza.furu@protonmail.ch>
2023-06-03 12:50:28 +0200

    extend feature extractor

--- backend/feature_extraction/feature_extractor.py
+++ backend/feature_extraction/feature_extractor.py
@@ -1,22 +1,57 @@
-
-
+import glob
+import os
+from pathlib import Path
+from typing import Optional, Union
+from PIL import Image
 import timm
 import torch
 import numpy as np
 import hdbscan
 from torch import nn
 from sklearn import decomposition
+from torchvision import transforms
+
+def mkdir(dir_path):
+    Path(dir_path).mkdir(parents=True, exist_ok=True)
 
 
 class FeatureExtractor(nn.Module):
-    def __init__(self, m: nn.Module):
+    """
+        Initial Feature Extractor
+    """
+
+    def __init__(self, img_path: Optional[Union[str, os.PathLike]], extractor: nn.Module, device: str = 'cpu'):
         super().__init__()
-        self.m = m
         self.pca = decomposition.PCA(n_components=2)
         self.hdbscan = hdbscan.HDBSCAN(min_cluster_size=3, gen_min_span_tree=True)
+        self.device = device
+        self.resize = transforms.Resize(size=(256, 256), interpolation=transforms.InterpolationMode.NEAREST)
+        if img_path:
+            self.load_imgs(img_path)
+        else:
+            self.imgs = None
+        self.extractor = extractor.to(device)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.m.forward_features(x)
+    def load_imgs(self, img_path: Union[str, os.PathLike]):
+        if os.path.isdir(img_path):
+            files = glob.glob(f'{img_path}/*.*')
+            arr = np.array([np.array(self.resize(Image.open(fname))) for fname in files])
+        else:
+            arr = np.array([np.array(self.resize(Image.open(img_path)))])
+        self.imgs = torch.tensor(np.transpose(arr, (0, 3, 1, 2)))
+
+    def _extract_features_preloaded(self) -> torch.tensor:
+        return self.extractor.forward_features(self.imgs)
+
+    def _extract_features(self, x: torch.Tensor) -> torch.tensor:
+        x = x.to(self.device)
+        return self.extractor.forward_features(x)
+
+    def forward(self, x: Optional[torch.Tensor] = None) -> torch.Tensor:
+        if x is None:
+            assert self.imgs, "Preload images or pass torch tensor"
+            return self.m._extract_features_preloaded(x)
+        return self._extract_features(x)
 
     @torch.inference_mode()
     def forward_project_pca(self, x: torch.Tensor) -> np.ndarray:
@@ -26,9 +61,12 @@ class FeatureExtractor(nn.Module):
         return y
 
 
-# PCR, umap
-def get_feature_extractor(name: str = 'convnext_base.clip_laion2b') -> FeatureExtractor:
-    m = timm.create_model(model_name=name, pretrained=True)
-    return FeatureExtractor(m)
-
+def get_feature_extractor(img_path: Optional[Union[str, os.PathLike]] = None,
+                          device: Optional[str] = None,
+                          extractor_name: str = 'convnext_base.clip_laion2b') -> FeatureExtractor:
+    extractor = timm.create_model(model_name=extractor_name, pretrained=True)
+    device = device or 'cuda' if torch.cuda.is_available() else 'cpu'
+    return FeatureExtractor(img_path=img_path,
+                            extractor= extractor,
+                            device=device)
 
