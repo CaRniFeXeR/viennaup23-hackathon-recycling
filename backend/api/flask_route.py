174cf089c6d99d9450e978d82c0024e20abc78c0
Florian Pfiel <florian.pfiel@ace.at>
2023-06-03 12:04:09 +0200

    updated endpoint get_latest_scans

--- backend/api/flask_route.py
+++ backend/api/flask_route.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, send_file, send_from_directory
+from PIL import Image
 import flask
+import os
 
 app = Flask(__name__)
 
@@ -21,20 +23,20 @@ app = Flask(__name__)
 # ]
 
 example_data = {
-        "img0.png": [0.23939520415773685, 0.39729491531822647],
-        "img1.png": [0.7109474876517918, 0.5464037739352541],
-        "img2.png": [0.4823443230341764, 0.1366062742947145],
-        "img3.png": [0.8467880824049828, 0.7541917936743278],
-        "img4.png": [0.09798797908084622, 0.6638587418518253],
-        "img5.png": [0.06131955485075535, 0.3103182742510159],
-        "img6.png": [0.09068516802941429, 0.47123703536053474],
-        "img7.png": [0.015828088631563042, 0.7393210213742311],
-        "img8.png": [0.7671876772668083, 0.453705845175297],
-        "img9.png": [0.6255427512261426, 0.19949894591375994],
-        "img10.png": [0.5123464696557279, 0.21399429733929098],
-        "img11.png": [0.5718495766885707, 0.9615837987658215],
-        "img12.png": [0.6701364557109918, 0.2437979932943165],
-        "img13.png": [0.3927738101131796, 0.5888332234460126]
+        "img0_0.png": [0.23939520415773685, 0.39729491531822647],
+        "img0_1.png": [0.7109474876517918, 0.5464037739352541],
+        "img0_2.png": [0.4823443230341764, 0.1366062742947145],
+        "img0_3.png": [0.8467880824049828, 0.7541917936743278],
+        "img2_0.png": [0.09798797908084622, 0.6638587418518253],
+        "img2_1.png": [0.06131955485075535, 0.3103182742510159],
+        "img2_2.png": [0.09068516802941429, 0.47123703536053474],
+        "img2_3.png": [0.015828088631563042, 0.7393210213742311],
+        "img2_4.png": [0.7671876772668083, 0.453705845175297],
+        "img3_0.png": [0.6255427512261426, 0.19949894591375994],
+        "img3_1.png": [0.5123464696557279, 0.21399429733929098],
+        "img3_2.png": [0.5718495766885707, 0.9615837987658215],
+        "img3_3.png": [0.6701364557109918, 0.2437979932943165],
+        "img3_4.png": [0.3927738101131796, 0.5888332234460126]
     }
 
 
@@ -49,10 +51,21 @@ def get_latest_scans(n):
     for img_name, point in example_data.items():
         if(counter >= n):
             break
-
+        
+        counter += 1
         x, y = point
-        img_url = "/static_content/" + img_name
-        img_size = [5, 5]
+        img_url = "./static_content/" + img_name
+        img_size = [0, 0]
+        script_dir = os.path.dirname(__file__)
+        #parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
+        file_path = os.path.join(script_dir, "..", "segmentation", "samples", img_name)
+
+        try:
+            with Image.open(file_path) as img:
+                img_size = img.size
+        except IOError:
+            continue
+
 
         target_obj = {
             'id': counter,
@@ -62,7 +75,6 @@ def get_latest_scans(n):
         }
 
         TargetArr.append(target_obj)
-        counter += 1
 
     return jsonify({'scanned_objects': TargetArr})
 
